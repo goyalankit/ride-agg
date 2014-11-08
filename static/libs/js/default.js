050346e119e2f442cf291c4245a6d5bce28d6e58
Ankit Goyal <ankit3goyal@gmail.com>
2014-11-08 15:53:07 -0600

    adding basic view

---
+++ static/libs/js/default.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(form)
+  .on('success.form.bv', function(e) {
+    // Prevent form submission
+    e.preventDefault();
+
+    // Get the form instance
+    var $form = $(e.target);
+
+    // Get the BootstrapValidator instance
+    var bv = $form.data('bootstrapValidator');
+
+    // Use Ajax to submit form data
+    $.post($form.attr('action'), $form.serialize(), function(result) {
+      // ... Process the result ...
+    }, 'json');
+  });
+});
